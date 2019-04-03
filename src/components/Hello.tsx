4ded93a5a9ba581196ae43e0635078900298cbae
Tony <tony@Tonys-MacBook-Pro.local>
2019-04-03 15:42:10 -0700

    init

---
+++ src/components/Hello.tsx
@@ -0,0 +1,6 @@
+// import React from 'react';
+import * as React from 'react';
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
